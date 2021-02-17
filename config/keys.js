67d96e196f06beb58fac73732f21c4a40d8a08c2
Rohit Shinde <62435594+rohit180301@users.noreply.github.com>
2021-02-17 09:35:28 +0530

    changes made

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV ==='production'){
-    module.export = require('./keys-product');
+    module.exports = require('./keys-product');
 }else{
     module.exports = require('./keys-dev');
 }
