0353d36ca8e0e78103a41ed050d82815dd5fb446
Rohit Shinde <62435594+rohit180301@users.noreply.github.com>
2021-02-17 13:46:01 +0530

    config keys added

--- config/keys-product.js
+++ config/keys-product.js
@@ -1,5 +1,5 @@
 module.exports={
-    MongoURI :'mongodb+srv://socialmediaapp:socialmediaapp1@onlineapp.k6gtv.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    GoogleClientID:'636145685348-lovmlctkpse1ovh5c2f7jihvvctsn13p.apps.googleusercontent.com',
-    GoogleClientSecret:'tqwnuv1dqrusXLn4bMgFyxXA'
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
